112458e872270c3c74d722931cac26de01aba3a0
root <root@motomami.localdomain>
2023-07-08 19:10:59 -0700

    pushing dev and production env files

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: require("find-config")(".env") });
 const ck = require("ckey");
 
-BE_ROOT = process.env.REACT_APP_BE_ROOT;
+const BE_ROOT = process.env.REACT_APP_BE_ROOT;
 module.exports = {
   BE_ROOT,
 };
