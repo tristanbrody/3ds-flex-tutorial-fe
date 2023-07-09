eb9d1e28babad4a53910ce890e840cc637eed3e1
root <root@motomami.localdomain>
2023-07-08 19:09:06 -0700

    pushing dev and production env files

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -2,17 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: require("find-config")(".env") });
 const ck = require("ckey");
 
-console.log(process.env.REACT_APP_BE_ROOT_TEST);
-let BE_ROOT =
-  "http://3ds-flex-tutorial-be-env.eba-ivqy6wrb.us-west-2.elasticbeanstalk.com";
-try {
-  BE_ROOT = process.env.REACT_APP_BE_ENV = "TEST"
-    ? process.env.REACT_APP_BE_ROOT_TEST
-    : "http://3ds-flex-tutorial-be-env.eba-ivqy6wrb.us-west-2.elasticbeanstalk.com";
-} catch {
-  BE_ROOT =
-    "http://3ds-flex-tutorial-be-env.eba-ivqy6wrb.us-west-2.elasticbeanstalk.com";
-}
+BE_ROOT = process.env.REACT_APP_BE_ROOT;
 module.exports = {
   BE_ROOT,
 };
