559b37ca23890b8ced491870658c4ac4fa3b6f2a
root <root@motomami.localdomain>
2023-07-08 19:02:19 -0700

    -

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -3,11 +3,12 @@ require("dotenv").config({ path: require("find-config")(".env") });
 const ck = require("ckey");
 
 console.log(process.env.REACT_APP_BE_ROOT_TEST);
-let BE_ROOT
+let BE_ROOT =
+  "http://3ds-flex-tutorial-be-env.eba-ivqy6wrb.us-west-2.elasticbeanstalk.com";
 try {
- BE_ROOT = (process.env.REACT_APP_BE_ENV = "TEST"
+  BE_ROOT = process.env.REACT_APP_BE_ENV = "TEST"
     ? process.env.REACT_APP_BE_ROOT_TEST
-    : process.env.REACT_APP_BE_ROOT_PROD);
+    : "http://3ds-flex-tutorial-be-env.eba-ivqy6wrb.us-west-2.elasticbeanstalk.com";
 } catch {
   BE_ROOT =
     "http://3ds-flex-tutorial-be-env.eba-ivqy6wrb.us-west-2.elasticbeanstalk.com";
